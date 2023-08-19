4c290fc88cbd628ce4ddd1d27b7d22f6691518e4
Javlon Akhundjanov <zha.akhundzhanov@x5.ru>
2023-08-19 16:00:28 +0300

    add: user info & user components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
-import st from './Sidebar.scss';
+import { UserInfo } from '../UserInfo';
+import st from './Sidebar.module.scss';
 
 export const Sidebar = () => {
 	return (
 		<aside className={st.sidebar}>
-			<></>
+			<UserInfo />
 		</aside>
 	);
 };
