d043bd8d1b2c595ff4c54ae4a8ffc98611b9dae7
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-08-19 16:07:56 +0300

    edit: user in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import { UserInfo } from '../UserInfo';
->>>>>>> 4c290fc (add: user info & user components)
 import st from './Sidebar.module.scss';
 
 export const Sidebar = () => {
