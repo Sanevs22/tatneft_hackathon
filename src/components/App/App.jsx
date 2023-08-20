c5d2b23716e11ffcf8e1f941e8ac3ed8dd4f5e8a
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-08-20 12:03:20 +0300

    add: link to demo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import { Sidebar } from '../Sidebar';
 import { Dashboard } from '../Dashboard';
 import { Main } from '../UI/Main';
 import st from './App.module.scss';
+import Demo from '../Demo/Demo';
 
 export const App = () => {
 	return (
 		<div className={st.App}>
-			<Sidebar></Sidebar>
-			<Main>
-				<Dashboard />
-			</Main>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<Sidebar />
+							<Main>
+								<Dashboard />
+							</Main>
+						</>
+					}
+				/>
+				<Route path='/' element={<Demo />} />
+			</Routes>
 		</div>
 	);
 };
