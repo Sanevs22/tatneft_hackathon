2af59274687c3b78e66391d6c97091ecc0f94334
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-08-20 12:10:53 +0300

    add: btn to demo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,24 @@
+import { Link } from 'react-router-dom';
 import { Info } from '../Info/Info';
 import { UserInfo } from '../UserInfo';
 import st from './Sidebar.module.scss';
 
 export const Sidebar = () => {
+	const btnStyle = {
+		background: '#fff',
+		border: '1px solid #4fd1c5',
+		width: '100%',
+		padding: '10px',
+		borderRadius: '10px',
+		marginTop: '24px',
+	};
 	return (
 		<aside className={st.sidebar}>
 			<UserInfo />
 			<Info placeholder='Введите текст' />
+			<Link to='/demo'>
+				<button style={btnStyle}>Demo</button>
+			</Link>
 		</aside>
 	);
 };
